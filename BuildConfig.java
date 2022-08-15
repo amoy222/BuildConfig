74207d7dac37b00377685c8981bf86c63048882d
amoy222 <111284809+amoy222@users.noreply.github.com>
2022-08-15 19:34:54 +0700

    Add files via upload

---
+++ BuildConfig.java
@@ -0,0 +1,18 @@
+/*
+ * Decompiled with CFR 0.0.
+ * 
+ * Could not load the following classes:
+ *  java.lang.Object
+ *  java.lang.String
+ */
+package com.higgs.domino;
+
+public final class BuildConfig {
+    public static final String APPLICATION_ID = "com.higgs.domino";
+    public static final String BUILD_TYPE = "release";
+    public static final boolean DEBUG = false;
+    public static final String FLAVOR = "dmno_release";
+    public static final int VERSION_CODE = 1870;
+    public static final String VERSION_NAME = "1.87";
+}
+
